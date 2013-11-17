087617c7cfc8e28b048004d2d07a69524429a5cc
panaman <panaman@geekempire.com>
2013-11-16 21:08:33 -0500

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,64 +1,28 @@
 class bro(
-  $ensure = 'running',
-  $pkg_require = 'true',
-  $mods = 'false',
-  $pkg_ensure = 'present',
-  $pkg_provider = $bro::params::pkg_provider,
-  $pkg_source = "$bro::params::pkg_source",
-  $basedir = '/opt/bro',
-  $logdir = '/var/opt/bro',
-  $logrotation = '3600',
-  $logexpire = '5',
-  $mindisk = '5',
-  $mailto = 'root@localhost',
-  $debug = '0',
-  $manager = $hostname,
-  $pfring = 'false',
-  $proxy = $hostname,
-  $interface = $::hostint,
-  $wkr1 = 'NA',
-  $wkr1_cpus = 'NA',
-  $wkr1_int = 'NA',
-  $wkr2 = 'NA',
-  $wkr2_cpus = 'NA',
-  $wkr2_int = 'NA',
-  $wkr3 = 'NA',
-  $wkr3_cpus = 'NA',
-  $wkr3_int = 'NA',
-  $wkr4 = 'NA',
-  $wkr4_cpus = 'NA',
-  $wkr4_int = 'NA',
-  $wkr5 = 'NA',
-  $wkr5_cpus = 'NA',
-  $wkr5_int = 'NA',
-  $wkr6 = 'NA',
-  $wkr6_cpus = 'NA',
-  $wkr6_int = 'NA',
-  $wkr7 = 'NA',
-  $wkr7_cpus = 'NA',
-  $wkr7_int = 'NA',
-  $wkr8 = 'NA',
-  $wkr8_cpus = 'NA',
-  $wkr8_int = 'NA',
-  $wkr9 = 'NA',
-  $wkr9_cpus = 'NA',
-  $wkr9_int = 'NA',
-  $wkr10 = 'NA',
-  $wkr10_cpus = 'NA',
-  $wkr10_int = 'NA',
+  $ensure       = $bro::params::ensure,
+  $pf_cid       = $bro::params::pf_cid,
+  $debug        = $bro::params::debug,
+  $mailto       = $bro::params::mailto,
+  $sitepolicy   = $bro::params::sitepolicy,
+  $logexpire    = $bro::params::logexpire,
+  $mindisk      = $bro::params::mindisk,
+  $logrotate    = $bro::params::logrotate,
+  $mods         = $bro::params::mods,
+  $pkg_ensure   = $bro::params::pkg_ensure,
+  $pkg_source   = $bro::params::pkg_source,
+  $basedir      = $bro::params::basedir,
+  $logdir       = $bro::params::logdir,
+  $logrotation  = $bro::params::logrotation,
+  $manager      = $bro::params::manager,
+  $proxy        = $bro::params::proxy,
+  $worker       = $bro::params::worker,
+  $pfring       = $bro::params::pfring,
+  $int          = $bro::params::interface,
+  $bro_pkg_name = $bro::params::bro_pkg_name,
+  $bro_url      = $bro::params::bro_url,
+  $type         = $bro::params::type,
+  $network      = $bro::params::network,
   ) inherits bro::params {
-  if ($wkr1_cpus != 'NA' ) {
-    if is_array($wrk1_cpus) {
-      $bro_firstcpu = values_at($wkr1_cpus, 0)
-      $bro_lastcpu = values_at($wkr1_cpus, 1)
-    } else {
-      $bro_firstcpu = $wkr1_cpus
-      $bro_lastcpu = $wkr1_cpus
-    }
-    $bro_cpu_range = range("$bro_firstcpu", "$bro_lastcpu")
-    $bro_cpu_pin = join($bro_cpu_range, ",")
-    $bro_cpu_count = count($bro_cpu_range)
-  } else {}
   if ( $ensure == 'running' ) {
     $bro_present = 'present'
     $bro_state  = 'running'
@@ -66,14 +30,8 @@ class bro(
     $bro_present = 'absent'
     $bro_state  = 'stopped'
   }
-  if ( $pkg_require == 'true' ) {
-    package { 'bro':
-      provider => $pkg_provider,
-      ensure   => $bro_present,
-      source   => $pkg_source,
-      notify => Service['wassup_bro'],
-    }
-  } else {}
+  class {'bro::pkg':}
+  class {'bro::broctl':}
   File {
     ensure => present,
     mode   => '0644',
@@ -114,9 +72,9 @@ class bro(
     mode => '0755',
     content => template('bro/wassup_bro.erb'),
   }
-  $status = "$basedir/bin/wassup_bro status | grep running"
-  $start = "$basedir/bin/wassup_bro start"
-  $stop = "$basedir/bin/wassup_bro stop"
+  $status  = "$basedir/bin/wassup_bro status | grep running"
+  $start   = "$basedir/bin/wassup_bro start"
+  $stop    = "$basedir/bin/wassup_bro stop"
   $restart = "$basedir/bin/wassup_bro restart"
   service { 'wassup_bro':
     ensure  => $bro_state,
@@ -132,15 +90,34 @@ class bro(
     user    => '0',
     minute  => '*/5',
   }
-  file { "${basedir}/etc/broctl.cfg":
-    content => template('bro/broctl.cfg.erb'),
-    notify  => Service['wassup_bro'],
+  $node_conf = "${basedir}/etc/node.cfg"
+  if ($type == 'cluster') {
+    concat { $node_conf:
+      owner => '0',
+      group => '0',
+      mode  => '0644',
+    }
+    concat::fragment { 'node_conf_header':
+      target  => $node_conf,
+      content => "# CONFIG MANAGED BY PUPPET\n\n",
+      order   => 01,
+    }
+    concat::fragment { 'node_conf_manager':
+      target  => $node_conf,
+      content => template('bro/manager_name.erb'),
+      order   => 02,
+    }
+    concat::fragment { 'node_conf_proxy':
+      target  => $node_conf,
+      content => template('bro/proxy_name.erb'),
+      order   => 03,
+    }
+  } else {
+    file { "${node_conf}":
+      content => template('bro/node.cfg.erb'),
+      notify  => Service['wassup_bro'],
+    }
   }
-  file { "${basedir}/etc/node.cfg":
-    content => template('bro/node.cfg.erb'),
-    notify  => Service['wassup_bro'],
-  }
-  ### need more work
   file { "${basedir}/etc/networks.cfg":
     content => template('bro/networks.cfg.erb'),
     notify  => Service['wassup_bro'],
@@ -154,6 +131,4 @@ class bro(
   file { '/opt/bro/share/bro/site/local-worker.bro':
     content => "# Worker\n",
   }
-
-
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,30 @@
 class bro::params {
-  $start = '/opt/bro/bin/broctl start'
-  $restart = '/opt/bro/bin/broctl restart'
-  $install = '/opt/bro/bin/broctl install'
-  $stop = '/opt/bro/bin/broctl stop'
-  $status = 'ps -ef | grep run-bro | grep -v grep'
-  $ensure = 'running'
-  $debug = '0'
-  $bro_dir = '/opt/bro'
-  $manager = $::hostname
-  $proxy   = $::proxy
-  $interface = undef
-  $worker1 = "$::hostname-$interface"
-  $pkg_provider = $::osfamily ? { 
-     'RedHat' => 'rpm',
-     'Debian' => 'apt',
+  $ensure       = 'running'
+  $pf_cid       = 'UNSET'
+  $debug        = '0'
+  $mailto       = 'root@localhost'
+  $sitepolicy   = 'local.bro'
+  $logexpire    = '30'
+  $mindisk      = '5'
+  $logrotate    = '3600'
+  $mods         = 'false'
+  $basedir      = '/opt/bro'
+  $logdir       = '/var/opt/bro'
+  $logrotation  = '3600'
+  $manager      = $::hostname
+  $interface    = $::hostint
+  $worker       = $::hostname
+  $pfring       = 'false'
+  $proxy        = $::hostname
+  $pkg_ensure   = 'present'
+  $pkg          = 'bro'
+  $worker1      = "$::hostname-$interface"
+  $pkg_source   = 'bro.org'
+  $type         = 'standalone'
+  $network      = $::hostint_ipv4_cidr 
+  $bro_pkg_name = $::osfamily ? {
+    'RedHat' => 'Bro-2.2-Linux-x86_64.rpm',
+    'Debian' => 'Bro-2.2-Linux-x86_64.deb',
   }
-  $pkg_source = $::osfamily ? {
-     'RedHat' => 'http://www.bro.org/downloads/release/Bro-2.2-Linux-x86_64.rpm',
-     'Debian' => 'http://www.bro.org/downloads/release/Bro-2.2-Linux-x86_64.deb',
-  }
+  $bro_url = 'http://www.bro.org/downloads/release'
 }
